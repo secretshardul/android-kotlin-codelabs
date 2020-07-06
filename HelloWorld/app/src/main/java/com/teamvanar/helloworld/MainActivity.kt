67375a06154dda796a3aeb07ee214cbdedb990ff
shardul <shardul.aeer@somaiya.edu>
2020-07-06 12:12:03 +0530

    [lesson] late variable initialization, adding images, app compatibility

--- HelloWorld/app/src/main/java/com/teamvanar/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/teamvanar/helloworld/MainActivity.kt
@@ -5,69 +5,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    /**Lifecycle methods
-     * Android doesn't have a main() method. Instead, lifecycle functions like onCreate() and
-     * onDelete() are run one after the other.
+
+    /** lateinit
+     * Tells compiler that variable will be initialized. It removes need of null checks.
      */
+    private lateinit var diceImage: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /**View objects
-         * Android displays items using View objects created by the view class. Button, TextView,
-         * ListView etc. are subclasses of View class. Many views form a hierarchy similar to HTML
-         * DOM. Android runtime inflates XML files- strings, layouts etc. into view objects.
-         */
-        /**setContentView()
-         * Allows activity to inflate layout
-         */
-        /**R file
-         * Generated during build. It contains integer references to resources. It's used to access
-         * assets under `/res`
-         */
         setContentView(R.layout.activity_main) // Inflates layout
+
+        // To improve performance call findViewById() only once and save value globally.
+        diceImage = findViewById(R.id.dice_image)
         val rollButton = findViewById<Button>(R.id.roll_button)
+
         // Generate and display random number between 1-6
         rollButton.setOnClickListener {
             rollDice()
         }
-
-        val countUpButton = findViewById<Button>(R.id.count_up_button)
-        // Increment value if it's numeric and less than 6
-        countUpButton.setOnClickListener {
-            countUp()
-        }
-
-        val resetButton = findViewById<Button>(R.id.reset_button)
-        // Reset value to 0
-        resetButton.setOnClickListener {
-            val resultText = findViewById<TextView>(R.id.result_text)
-            resultText.text = "0"
-        }
-
     }
 
     private fun rollDice() {
-        // Context object allows us to communicate with and update state of android OS
-        // 'this' is passed because Activity and AppCompatActivity are subclasses of Context
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText = findViewById<TextView>(R.id.result_text)
-        resultText.text = (1..6).random().toString()
-    }
 
-    fun String.intOrString() = toIntOrNull() ?: this
-
-    private fun countUp() {
-
-        val resultText = findViewById<TextView>(R.id.result_text)
-        var resultNumber = resultText.text.toString().toIntOrNull()
-        if (resultNumber != null) {
-            if (resultNumber < 6) {
-                resultText.text = (++resultNumber).toString()
-            }
+        val drawableImage = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableImage)
     }
 }
--- README.md
+++ README.md
@@ -3,8 +3,19 @@
 ## How to use
 Explanation given in comment format. Follow repos in order.
 1. (HelloWorld)[/HelloWorld]: Dice roll app
-    1. Lifecycle methods
-    2. Layout inflation, View classes and objects
-    3. File structure: activities, layouts, R file, Android manifest
-    4. Listeners and callback methods
-    5. Layouts: Linear layout, orientation, layout gravity
+    1. Android app anatomy
+        1. Lifecycle methods
+        2. Layout inflation, View classes and objects
+        3. File structure: activities, layouts, R file, Android manifest
+        4. Listeners and callback methods
+        5. Layouts: Linear layout, orientation, layout gravity
+        
+    2. `lateinit`, adding images, compatibility
+        1. ```lateinit var```: Minimize the calls to `findViewById()`. Allows global view references and late initialization while removing need of null checks.
+        2. ```tools:src```: Add placeholder image in layout file which is only visible in IDE. Its not added into app on compilation. Its helpful for development.
+        3. Android SDK versions in `build.gradle(:app)`:
+            - **compileSdkVersion**: Version used to compile app. It allows app to use new features.
+            - **targetSdkVersion**: Most recent API you have tested your app against.
+            - **minSdkVersion**: Minimum level to support.
+        4. Backward compatibility
+            **androidx** provides helper classes to ensure backward compatibility. It's an improvement over Android support library/
