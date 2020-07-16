777e32bd607494af7f944314e005153da66a7805
shardul <shardul.aeer@somaiya.edu>
2020-07-16 19:40:18 +0530

    [contd][feat] RecyclerView with Data Binding

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,10 +1,7 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -12,9 +9,10 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
 /** Constructor is set as private because from() function acts as constructor and returns ViewHolder object **/
-class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(itemView) {
+class ViewHolder private constructor(private val binding: ListItemSleepNightBinding): RecyclerView.ViewHolder(binding.root) {
     companion object {
         /**
          * Inflate XML layout and return as ViewHolder object.
@@ -24,14 +22,10 @@ class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(it
          */
         fun from(parent: ViewGroup): ViewHolder {
             val layoutInflater = LayoutInflater.from(parent.context) // Used to inflate XML layouts
-            val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
-            return ViewHolder(view)
+            val binding = ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
+            return ViewHolder(binding)
         }
     }
-
-    private val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-    private val quality: TextView = itemView.findViewById(R.id.quality_string)
-    private val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
 
     /**
      * Called by Adapter to bind data to inflated view at given position
@@ -39,9 +33,9 @@ class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(it
      */
     fun bind(item: SleepNight) {
         val res = itemView.context.resources
-        sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        quality.text = convertNumericQualityToString(item.sleepQuality, res)
-        qualityImage.setImageResource(when (item.sleepQuality) {
+        binding.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+        binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
+        binding.qualityImage.setImageResource(when (item.sleepQuality) {
             0 -> R.drawable.ic_sleep_0
             1 -> R.drawable.ic_sleep_1
             2 -> R.drawable.ic_sleep_2
